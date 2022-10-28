bb0856cb4e752d3e9d85814bf0e7050edf26e8c1
Shelsy Garcia <shelsyangelik@gmail.com>
2022-10-27 21:59:33 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,60 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from './lazy';
 
-console.log('Happy hacking :)')
+const  randomImage = () => {
+    const max = 123;
+    const min = 1;
+    let random =  Math.floor(Math.random() * (max - min) + min);
+    
+    return random
+}
+    
+const createImageNode = () =>{
+    
+    const container = document.createElement('div')
+    container.className = "p-4 "
+
+
+    //crear imagen
+    const image = document.createElement('img');
+    //agregamos el contenido, le damos la url de la imagen
+    image.dataset.src = `https://randomfox.ca/images/${randomImage()}.jpg`
+
+    image.className = "mx-auto  rounded-md bg-gray-300"
+    image.width="320"
+
+    
+
+
+    
+
+    container.appendChild(image);
+
+    return container;
+
+}
+
+const mountNode = document.querySelector('#images')
+
+//boton de añadir
+const addButton =  document.querySelector('#addbutton')
+addButton.className = "m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+
+const addImage = () => {
+    
+    const newImage = createImageNode();
+    mountNode.appendChild(newImage);
+    // ademas de agregar la imagen en el DOM, le decimos que la empiece a escuchar
+    registerImage(newImage);
+};
+
+addButton.addEventListener('click', addImage);
+
+//botton limpiar 
+const cleanButton =  document.querySelector('#cleanButton');
+cleanButton.className = "bg-transparent-700 hover:bg-white text-blue-500  border border-blue-500  font-bold py-2 px-4 rounded-full"
+
+const clean = () => {
+    mountNode.innerHTML= "";
+}
+
+cleanButton.addEventListener('click', clean);
